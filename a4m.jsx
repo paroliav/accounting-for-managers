ee85c05bc2909a6bfa14bbc1cc9c5fdc0ab257de
Vivek Parolia <paroliaviv@gmail.com>
2021-02-19 01:05:27 +1100

    Cash Conversion cycle formulas added

--- a4m.jsx
+++ a4m.jsx
@@ -1,54 +1,79 @@
 function calculate() {
-            document.getElementById('pM').innerHTML = pM()
-            document.getElementById('aT').innerHTML = aT()
-            document.getElementById('leverage').innerHTML = leverage()
-            document.getElementById('roa').innerHTML = roa()
-            document.getElementById('roe').innerHTML = roe()
+    document.getElementById('pM').innerHTML = pM()
+    document.getElementById('aT').innerHTML = aT()
+    document.getElementById('leverage').innerHTML = leverage()
+    document.getElementById('roa').innerHTML = roa()
+    document.getElementById('roe').innerHTML = roe()
 
-            document.getElementById('roa_npat').innerHTML = roa_npat()
-            document.getElementById('roa_ebit').innerHTML = roa_ebit()
-            document.getElementById('roe_npat').innerHTML = roe_npat()
-            document.getElementById('roe_ebit').innerHTML = roe_ebit()
-            document.getElementById('pM_npat').innerHTML = pM()
-            document.getElementById('pM_ebit').innerHTML = pM_ebit()
-        }
+    document.getElementById('roa_npat').innerHTML = roa_npat()
+    document.getElementById('roa_ebit').innerHTML = roa_ebit()
+    document.getElementById('roe_npat').innerHTML = roe_npat()
+    document.getElementById('roe_ebit').innerHTML = roe_ebit()
+    document.getElementById('pM_npat').innerHTML = pM()
+    document.getElementById('pM_ebit').innerHTML = pM_ebit()
+}
 
-        let aT = () => {
-          return document.getElementById('sales').value / document.getElementById('assets').value;
-        }
+let aT = () => {
+  return document.getElementById('sales').value / document.getElementById('assets').value;
+}
 
-        let pM = () => {
-            return document.getElementById('npat').value / document.getElementById('sales').value;
-        }
+let pM = () => {
+    return document.getElementById('npat').value / document.getElementById('sales').value;
+}
 
-        let leverage = () => {
-            return document.getElementById('assets').value / document.getElementById('equity').value;
-        }
+let leverage = () => {
+    return document.getElementById('assets').value / document.getElementById('equity').value;
+}
 
-        let roa = () => {
-         return aT() * pM();
-        }
+let roa = () => {
+ return aT() * pM();
+}
 
-        let roe = () => {
-         return roa() * leverage();
-        }
+let roe = () => {
+ return roa() * leverage();
+}
 
-        let roa_npat = () => {
-         return document.getElementById('npat').value / document.getElementById('assets').value;
-        }
+let roa_npat = () => {
+ return document.getElementById('npat').value / document.getElementById('assets').value;
+}
 
-        let roa_ebit = () => {
-         return document.getElementById('ebit').value / document.getElementById('assets').value;
-        }
+let roa_ebit = () => {
+ return document.getElementById('ebit').value / document.getElementById('assets').value;
+}
 
-        let roe_npat = () => {
-         return document.getElementById('npat').value / document.getElementById('equity').value;
-        }
+let roe_npat = () => {
+ return document.getElementById('npat').value / document.getElementById('equity').value;
+}
 
-        let roe_ebit = () => {
-         return document.getElementById('ebit').value / document.getElementById('equity').value;
-        }
+let roe_ebit = () => {
+ return document.getElementById('ebit').value / document.getElementById('equity').value;
+}
 
-        let pM_ebit = () => {
-         return document.getElementById('ebit').value / document.getElementById('sales').value;
-        }
+let pM_ebit = () => {
+ return document.getElementById('ebit').value / document.getElementById('sales').value;
+}
+
+function ccc() {
+    document.getElementById('nWC').innerHTML = nWC()
+    document.getElementById('rT').innerHTML = rT()
+    document.getElementById('iT').innerHTML = iT()
+    document.getElementById('dInR').innerHTML = dInR()
+}
+
+let nWC = () => {
+    return document.getElementById('cA').value - document.getElementById('cL').value;
+}
+
+let rT = () => {
+    const avgReceivables = (document.getElementById('oR').value + document.getElementById('cR').value) / 2;
+    return document.getElementById('sales_ccc').value / avgReceivables;
+}
+
+let iT = () => {
+    const avgInventory = (document.getElementById('oI').value + document.getElementById('cI').value) / 2;
+    return document.getElementById('cogs_ccc').value / avgInventory;
+}
+
+let dInR = () => {
+    return 365 / rT();
+}
